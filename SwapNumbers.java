6b7041caa5c5fddc75c112a25271ef9b6593d564
Snehil Patel <snehilpatel@L-JHYR4J49YP.local>
2023-01-17 12:37:43 +0530

    First code

---
+++ SwapNumbers.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class SwapNumbers
+{
+	public static void main(String[] args) {
+		System.out.println("Enter two numbers x and y: ");
+		Scanner sc = new Scanner(System.in);
+		int x=sc.nextInt();
+		int y=sc.nextInt();
+		
+		x = x + y;   
+        y = x - y;   
+        x = x - y;   
+        System.out.println("After swapping: "+x +"  " + y);
+	}
+}
